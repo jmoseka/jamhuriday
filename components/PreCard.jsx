919722b7a8203305d51f50ee2184c0f01e938d78
jmoseka <jamylam3@gmail.com>
2023-12-03 14:45:09 +0300

    structure pre colonial section

--- components/PreCard.jsx
+++ components/PreCard.jsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 
-export default function PreCard({ title, desc, img, align }) {
+export default function PreCard({ title, desc, img, align, order }) {
   return (
-    <div className="border">
-      <div className="md:flex md:justify-between md:items-center">
-        <div className="md:h-[1px] md:w-28 md:bg-red-400"></div>
-        <h2 className={` text-2xl `}> {title}</h2>
+    <div className="w-full flex flex-col md:flex-row gap-2 md:gap-2 md:items-center">
+      <div className={` text-start md:w-[50%] md:text-center md:order-${order}`}>
+        <h2 className={` text-2xl md:text-3xl `}> {title}</h2>
       </div>
 
-      <p className={`border pt-1 md:pt-8 pb-3 w-full ${align}`}>{desc}</p>
-      <div className="md:w-full md:pt-8">
-        <div className="border md:mx-auto border-white md:w-44">
+      <div className={`flex flex-col gap-5 md:w-1/2`}>
+        <p className={``}>{desc}</p>
+
+        <div className="border md:mx-auto border-green-700 md:w-44">
           <Image width={50} height={200} src="" alt="image" />
         </div>
       </div>

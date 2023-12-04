8a5b01dfc7facc919be38501a047669c6be7187e
jmoseka <jamylam3@gmail.com>
2023-12-04 13:44:26 +0300

    complete core functionalities

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+"use client"
 import IntroText from '@/components/IntroText'
 import PreCard from '@/components/PreCard'
+import PreSection from '@/components/PreSection';
 import PresentCard from '@/components/PresentCard';
 import ResistantCard from '@/components/ResistantCard'
 import Image from 'next/image'
+import { useState } from 'react';
+
 
 export default function Home() {
   const mauMauText = `The Mau Mau movement was a significant anti-colonial uprising in Kenya during the 1950s against British colonial rule.
@@ -11,6 +15,25 @@ export default function Home() {
   The Mau Mau movement was characterized by guerrilla warfare, strikes, protests, and acts of defiance against the British authorities and their loyalist allies.
   The British responded to the uprising with a heavy-handed crackdown, declaring a state of emergency in 1952. They carried out mass arrests, detentions, torture, and executions.
   The Mau Mau uprising ultimately played a crucial role in Kenya's struggle for independence. It brought international attention to the injustices of colonialism and contributed significantly to the push for Kenya's eventual independence, which was achieved in 1963.`;
+
+  const [images, setImages] = useState([
+    { id: 1, src: '' },
+    { id: 2, src: '' },
+    { id: 3, src: '' },
+    { id: 4, src: '' },
+  ]);
+
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+  const smallimages = images.slice(1, 4);
+
+  const handleClick = (image) => {
+    setSelectedImage(image);
+    setImages((prevImages) => {
+      const updatedImages = prevImages.filter((img) => img.id !== image.id);
+      return [image, ...updatedImages];
+    });
+  };
+
   return (
     <main className="flex min-h-screen w-full 
     flex-col items-center justify-between px-3 md:px-20 font-istok">
@@ -33,30 +56,22 @@ export default function Home() {
           desc="This timeline explores the rich and diverse history of the region that is now Kenya before the arrival of Europeans. It delves into the various ethnic groups, their cultures, and the way of life before colonialism."
         />
 
-        <div className='pt-20 section-body flex flex-col gap-20 md:gap-32  '>
+        <div className='pt-20 section-body flex flex-col gap-20 md:gap-20  '>
 
-          <article className='md:self-end section-body-content '>
+          <PreSection />
 
-            <PreCard
-              title='Social Structures'
-              desc='Several indigenous societies and kingdoms flourished, such as the Kikuyu, Kamba, Luo, Maasai, and many others. They had had well-defined social structures. Evidence of chiefs, elders, and councils for decision-making and conflict resolution can be found in historical accounts and oral traditions.'
-            />
-          </article>
-
-          <article className='w-full section-body-content'>
+          {/* <article className='w-full section-body-content'>
 
             <PreCard
               order='2'
               title='Religion and Spirtuality'
               desc='Indigenous belief systems and spiritual practices were an integral part of African traditional societies. Evidence of sacred sites, rituals, and religious artifacts demonstrates their spiritual traditions.'
             />
-          </article>
+          </article> */}
 
         </div>
 
-        <div className='pt-12  self-end'>
-          <button type='button' className='w-14 h-14 border rounded-full text-white text-sm'>NEXT</button>
-        </div>
+
 
       </section>
 
@@ -156,24 +171,21 @@ export default function Home() {
 
             <div className='col-span-3'>
               <figure>
-                <Image className=' w-full h-[14rem] md:h-[22rem]' width={200} src="" alt="image" />
+                <p>paragraph {selectedImage.id}</p>
+                <Image className=' w-full h-[14rem] md:h-[22rem]' width={200} src={selectedImage.src} alt="image" />
                 <figcaption>
-                  (Original Caption) 6/1/1963-Nairobi, Kenya- Waving his wisk the newly-elected Premier of Kenya, Jomo Kenyatta, (R, foreground), greeted throngs of cheering citizens as he rode through the streets of Nairobi. Accompanying Kenyatta are Tom Mboya, (L), Minister of Justice and Constitutional Affairs; A. Oginga Odinga, Minister for Home Affairs; and James S. Gichuru, Minister for Finance. 
+                  (Original Caption) 6/1/1963-Nairobi, Kenya- Waving his wisk the newly-elected Premier of Kenya, Jomo Kenyatta, (R, foreground), greeted throngs of cheering citizens as he rode through the streets of Nairobi. Accompanying Kenyatta are Tom Mboya, (L), Minister of Justice and Constitutional Affairs; A. Oginga Odinga, Minister for Home Affairs; and James S. Gichuru, Minister for Finance.
                 </figcaption>
               </figure>
             </div>
 
-
-            <div className='h-36 w-full border'>
-
-            </div>
-
-            <div className='h-36 w-full border'>
-
-            </div>
-
-            <div className='h-36 w-full border'>
-
+            <div className="image-list flex">
+              {smallimages.map((image) => (
+                <button key={image.id} className="small-image h-36 w-full border" onClick={() => handleClick(image)}>
+                  <p>Number {image.id}</p>
+                  <Image src={image.src} alt={`Image ${image.id}`} width={300} height={300} />
+                </button>
+              ))}
             </div>
 
 
@@ -206,7 +218,7 @@ export default function Home() {
 
           />
           <PresentCard
-          order={2}
+            order={2}
             title='The Constitution'
             desc='Kenya has had its own constitution since achieving independence. The countrys constitution outlines the framework for government, the separation of powers, and the rights and responsibilities of its citizens.
           '
---
+++ components/PreSection.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+import PreCard from '@/components/PreCard'
+
+function PreSection() {
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const cardsPerPage = 2;
+    const totalCards = 4;
+    const totalPages = Math.ceil(totalCards / cardsPerPage);
+
+    const nextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1)
+        }
+    }
+
+    const prevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1)
+        }
+    }
+
+    const renderCard = () => {
+        const startIndex = (currentPage - 1) * cardsPerPage;
+        const endIndex = Math.min(startIndex + cardsPerPage, totalCards);
+
+        const cardsToShow = [];
+        for (let i = startIndex; i < endIndex; i++) {
+            cardsToShow.push(
+                <article className='md:self-end section-body-content '>
+
+                    <PreCard
+                    order = {i % 2 === 0 ? '2' : ''}
+                        title={`Social Structures ${i}`}
+                        desc='Several indigenous societies and kingdoms flourished, such as the Kikuyu, Kamba, Luo, Maasai, and many others. They had had well-defined social structures. Evidence of chiefs, elders, and councils for decision-making and conflict resolution can be found in historical accounts and oral traditions.'
+                    />
+                </article>
+            );
+        }
+        return cardsToShow;
+    }
+
+    return (
+        <>
+            {renderCard()}
+
+            <div className={`pt-12 flex border  ${currentPage === 1 ? 'justify-end' : 'justify-start'}`}>
+
+                {
+                    currentPage === 1 ?
+                        <div className='w-fit'>
+                            <button onClick={() => nextPage()} type='button' className='w-14 h-14 border rounded-full text-white text-sm'>NEXT</button>
+                        </div>
+                        :
+                        <div>
+                            <button onClick={() => prevPage()} type='button' className='w-14 h-14 border rounded-full text-white text-sm'>PREV</button>
+                        </div>
+                }
+
+
+            </div>
+        </>
+    )
+}
+
+export default PreSection;
